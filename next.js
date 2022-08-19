e22efcac7f7f74649c3cfd0d25ee25a282b2625e
Rudraprasad Das <brayn003@gmail.com>
2022-08-20 05:13:36 +0530

    adding css

--- next.js
+++ next.js
@@ -2,7 +2,8 @@ const commonConfig = require('./helpers/common-config');
 const overided = require('./helpers/overided-rules');
 
 module.exports = {
-	extends: ['airbnb', 'airbnb/hooks', 'plugin:css/standard'],
+	extends : ['airbnb', 'airbnb/hooks', 'plugin:css/standard'],
+	plugins : ['css'],
 
 	...commonConfig,
 
