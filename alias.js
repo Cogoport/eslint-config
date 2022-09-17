9ed5183336215ef57224b54cd6856d4b54a0527b
Rudraprasad Das <brayn003@gmail.com>
2022-09-17 22:24:21 +0530

    adding different entity alias

---
+++ alias.js
@@ -0,0 +1,10 @@
+module.exports = {
+	settings: {
+		'import/resolver': {
+			'eslint-import-resolver-custom-alias': {
+				alias      : { '@': './' },
+				extensions : ['.js', '.ts', '.tsx'],
+			},
+		},
+	},
+};
