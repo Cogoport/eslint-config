4eb618da1090fa59d909340a03312a091d9065d5
prashantp01 <prashant.paddune@cogoport.com>
2023-05-23 15:32:21 +0530

    fix
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

--- cogo-admin-custom.js
+++ cogo-admin-custom.js
@@ -2,7 +2,7 @@ const overrides = require('./overrides');
 
 module.exports = {
 	extends       : ['airbnb-base'],
-	plugins 	  : ['custom-eslint-rules'],
+	plugins    	  : ['eslint-plugin-custom-eslint-rules'],
 	parserOptions : { ecmaVersion: 2020 },
 	rules         : {
 		...overrides.cogoAdmin,
