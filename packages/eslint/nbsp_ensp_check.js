bfdd4262a8fe592a0028bc79bcf2b381a2b6bd7f
Rajatuba-Cogoport <rajat.uba@cogoport.com>
2023-06-22 19:38:55 +0530

    fix

--- packages/eslint/nbsp_ensp_check.js
+++ packages/eslint/nbsp_ensp_check.js
@@ -1,30 +1,27 @@
 module.exports = {
-	rules: {
-		'nbsp-ensp-check': {
-			meta: {
-				type : 'warn',
-				docs : {
-					description : 'Disallow the usage of non-breaking space or en space',
-					category    : 'Best Practices',
-					recommended : true,
-				},
-				schema: [],
-			},
-
-			create(context) {
-				return {
-					JSXText(node) {
-						const hasNonBreakingSpace = (/\u00A0|\u2002/g).test(node.value);
-
-						if (hasNonBreakingSpace) {
-							context.report({
-								node,
-								message: 'Non-breaking space (nbsp;) is not allowed. Use string literal (``) instead.',
-							});
-						}
-					},
-				};
-			},
+	meta: {
+		type : 'warn',
+		docs : {
+			description : 'Disallow the usage of non-breaking space or en space',
+			category    : 'Best Practices',
+			recommended : true,
 		},
+		schema: [],
 	},
+
+	create(context) {
+		return {
+			JSXText(node) {
+				const hasNonBreakingSpace = (/\u00A0|\u2002/g).test(node.value);
+
+				if (hasNonBreakingSpace) {
+					context.report({
+						node,
+						message: 'Non-breaking space (nbsp;) is not allowed. Use string literal (``) instead.',
+					});
+				}
+			},
+		};
+	},
+
 };
