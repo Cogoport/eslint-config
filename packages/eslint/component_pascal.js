da43af578afc6ef94d057c104dc987e08e5671b7
Shweta Kishor Donode <shweta@Shwetas-MacBook-Air.local>
2023-06-26 12:33:43 +0530

    fix: adding rules

---
+++ packages/eslint/component_pascal.js
@@ -0,0 +1,28 @@
+module.exports = {
+	meta: {
+		type : 'warn',
+		docs : {
+			description : 'Enforce PascalCase for React component names',
+			category    : 'Best Practices',
+			recommended : true,
+		},
+		fixable : 'code',
+		schema  : [],
+	},
+	create(context) {
+		return {
+			ReturnStatement(node) {
+				if (node.argument.type === 'JSXElement') {
+					const isPascal = /^([A-Z][a-z0-9]*)+$/.test(node.parent.parent.parent.id.name);
+					if (!isPascal) {
+						context.report({
+							node,
+							message : 'React component names should be in PascalCase.',
+							loc     : node.parent.parent.parent.loc,
+						});
+					}
+				}
+			},
+		};
+	},
+};
--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,5 +1,6 @@
 const check_element_role_button = require('./check_element_role_button');
 const cl_for_merging_styles_class = require('./cl_for_merging_styles_class');
+const component_pascal = require('./component_pascal');
 const custom_rules_matching = require('./custom_rules_matching');
 const date_time_format_check = require('./date_time_format_check');
 const email_check = require('./email_check');
@@ -21,5 +22,6 @@ module.exports = {
 		'cl-for-merging-styles-class' : cl_for_merging_styles_class,
 		'date-time-format-check'      : date_time_format_check,
 		'check-element-role-button'   : check_element_role_button,
+		'component-pascal'            : component_pascal,
 	},
 };
