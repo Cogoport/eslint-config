181f21f9cbfade70992bdedd49b36f85356986ea
prashantp01 <prashant.paddune@cogoport.com>
2023-06-30 13:41:38 +0530

    🔀 refactor(custom-eslint): move custom eslint rules to a separate 'rules' directory for better organization and maintainability 🔀 refactor(custom-eslint): rename 'custom-eslint-matching' rule to 'variables-name-check' for better clarity and consistency
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

--- packages/custom-eslint/index.js
+++ packages/custom-eslint/index.js
@@ -1,26 +1,26 @@
-const check_element_role_button = require('./check_element_role_button');
-const cl_for_merging_styles_class = require('./cl_for_merging_styles_class');
-const component_pascal = require('./component_pascal');
-const custom_rules_matching = require('./custom_rules_matching');
-const date_time_format_check = require('./date_time_format_check');
-const default_comp_props = require('./default_comp_props');
-const default_usestate = require('./default_usestate');
-const email_check = require('./email_check');
-const hook_sequence = require('./hook_sequence');
-const img_src_cdn = require('./img_src_cdn');
-const is_empty_use_check = require('./is_empty_use_check');
-const key_as_function = require('./key_as_function');
-const key_in_for_each_map = require('./key_in_for_each_map');
-const nbsp_ensp_check = require('./nbsp_ensp_check');
-const regex_check = require('./regex_check');
-const uuid_check = require('./uuid_check');
-const variable_name_check = require('./variable_name_check');
-const variable_value_jsx = require('./variable_value_jsx');
-const zeroth_index_import = require('./zeroth_index_import');
+const check_element_role_button = require('./rules/check_element_role_button');
+const cl_for_merging_styles_class = require('./rules/cl_for_merging_styles_class');
+const component_pascal = require('./rules/component_pascal');
+const date_time_format_check = require('./rules/date_time_format_check');
+const default_comp_props = require('./rules/default_comp_props');
+const default_usestate = require('./rules/default_usestate');
+const email_check = require('./rules/email_check');
+const hook_sequence = require('./rules/hook_sequence');
+const img_src_cdn = require('./rules/img_src_cdn');
+const is_empty_use_check = require('./rules/is_empty_use_check');
+const key_as_function = require('./rules/key_as_function');
+const key_in_for_each_map = require('./rules/key_in_for_each_map');
+const nbsp_ensp_check = require('./rules/nbsp_ensp_check');
+const regex_check = require('./rules/regex_check');
+const uuid_check = require('./rules/uuid_check');
+const variable_name_check = require('./rules/variable_name_check');
+const variable_value_jsx = require('./rules/variable_value_jsx');
+const variables_name_check = require('./rules/variables_name_check');
+const zeroth_index_import = require('./rules/zeroth_index_import');
 
 module.exports = {
 	rules: {
-		'custom-eslint-matching'       : custom_rules_matching,
+		'variables-name-check'        : variables_name_check,
 		'img-src-cdn'                 : img_src_cdn,
 		'regex-check'                 : regex_check,
 		'nbsp-ensp-check'             : nbsp_ensp_check,
